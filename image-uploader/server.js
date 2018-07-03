b64370f37cae304daaeb8a34bdd0073a6826d4d1
mcortez <cortezmartin1983@gmail.com>
2018-07-03 01:29:34 -0300

    Refactor para export del server, agrego index.js

--- image-uploader/server.js
+++ image-uploader/server.js
@@ -1,13 +1,24 @@
 var http = require("http"); //solicita el modulo http a node
 
-//Llamamos al metodo createServer
-function onRequest(request, response){
-  console.log("Peticion recibida.")
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("Hola Mundo");
-  response.end();
+function iniciar() {
+  function onRequest(request, response){
+    console.log("Peticion recibida.");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Hola Mundo");
+    response.end();
+    console.log(request.headers);
+  }
+
+  /*
+  Llamamos al metodo createServer:
+  createServer nos retorna un objeto que contiene el metodo listen donde indicamos
+  el puerto de escucha
+  */
+  http.createServer(onRequest).listen(8888);
+
+  console.log("Servidor iniciado.");
 }
-
-http.createServer(onRequest).listen(8888); //createServer nos retorna un objeto que contiene el metodo listen donde indicamos el puerto de escucha
-
-console.log("Servidor iniciado.");
+/*
+exports Permite exportar el server para ser llamado desde otro modulo
+*/
+exports.iniciar = iniciar;
