1a3695ee5105c3a462186aff0f0d337ad90e83bb
mcortez <cortezmartin1983@gmail.com>
2018-07-03 00:54:08 -0300

    Refactor server.js

--- image-uploader/server.js
+++ image-uploader/server.js
@@ -1,8 +1,13 @@
 var http = require("http"); //solicita el modulo http a node
 
 //Llamamos al metodo createServer
-http.createServer(function(request, response){
+function onRequest(request, response){
+  console.log("Peticion recibida.")
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write("Hola Mundo");
   response.end();
-}).listen(8888); //createServer nos retorna un objeto que contiene el metodo listen donde indicamos el puerto de escucha
+}
+
+http.createServer(onRequest).listen(8888); //createServer nos retorna un objeto que contiene el metodo listen donde indicamos el puerto de escucha
+
+console.log("Servidor iniciado.");
---
+++ pruebas/funcionComoParametro.js
@@ -0,0 +1,19 @@
+/*
+El siguiente ejemplo muestra el pasaje de una funcion como parametro de
+otra funcion
+*/
+function decir(palabra) {
+  console.log(palabra);
+}
+
+function ejecutar(algunaFuncion, valor) {
+  algunaFuncion(valor);
+} // decir se "convierte" en algunaFuncion al ser pasada como parametro
+
+ejecutar(decir, "Hola!");
+
+/*
+Como somos unos locos, y JS nos deja, podemos directamente usar la palaba
+reservada function para crear una funcion anonima y pasarla como parametro
+*/
+ejecutar(function(palabra){ console.log(palabra);}, "Hola desde la funcion anonima!")
